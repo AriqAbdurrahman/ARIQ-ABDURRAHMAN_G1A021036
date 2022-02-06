e50896afa67d1ba80e233317986b6f47169e7b27
AriqAbdurrahman <99084478+AriqAbdurrahman@users.noreply.github.com>
2022-02-06 12:50:52 +0700

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   double d1, d2, a, b, K, L;
+
+   cout<<"\t\t\tMENGHITUNG KELILING"<<endl;
+
+   cout<<"Masukan sisi a = ";
+   cin>>a;
+   cout<<"Masukan sisi b = ";
+   cin>>b;
+   cout<<"===============================\n"<<endl;
+
+   cout<<"Rumus Keliling Layang-Layang = 2*(a+b)"<<endl;
+   K=2*(a+b);
+
+   cout<<"Keliling = "<<K<<endl;
+}
